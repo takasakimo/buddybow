500a317d038114d2b0aab148589b224b53b6170c
あなたの名前 <@example.com>
2025-11-20 11:27:13 +0900

    サイドバーとコンテンツの重なりを修正

--- components/layout/DashboardLayout.tsx
+++ components/layout/DashboardLayout.tsx
@@ -8,9 +8,9 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex min-h-screen bg-gray-50">
+    <div className="min-h-screen bg-gray-50">
       <Sidebar />
-      <main className="flex-1 lg:ml-64 p-4 lg:p-8">
+      <main className="pt-4 pl-20 pr-4 pb-8">
         {children}
       </main>
     </div>
