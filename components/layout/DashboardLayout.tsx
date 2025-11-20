855e95548e55f01f8efc766704b2c1222322dc89
あなたの名前 <@example.com>
2025-11-20 11:30:51 +0900

    コンテンツの位置を修正

--- components/layout/DashboardLayout.tsx
+++ components/layout/DashboardLayout.tsx
@@ -10,7 +10,7 @@ export default function DashboardLayout({
   return (
     <div className="min-h-screen bg-gray-50">
       <Sidebar />
-      <main className="pt-4 pl-20 pr-4 pb-8">
+      <main className="pt-20 px-4 pb-8">
         {children}
       </main>
     </div>
